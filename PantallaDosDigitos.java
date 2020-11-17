d1025e3facaff63272c208121ffd5993138410ac
pablo <pablomrmr@gmail.com>
2020-11-17 09:39:50 +0100

    0167.02 nuevo metodo incrementaValorAlmacenado

--- PantallaDosDigitos.java
+++ PantallaDosDigitos.java
@@ -61,9 +61,6 @@ public class PantallaDosDigitos
      * Incrementa el valor de la pantalla. Si llega al limite cambia a uno.
      */
     public void incrementaValorAlmacenado() {
-        valorAlmacenado += 1;
-        if (valorAlmacenado >= valorLimite){
-            valorAlmacenado = valorInicial;
-        }
+        valorAlmacenado =  (((valorAlmacenado + 1) - valorInicial) % (valorLimite - valorInicial)) + valorInicial;
     }
 }
